b4ee0fe76a8afa67cbe8bd5ed709c7b34b4f08d6
Israel Murat Corrêa <israelmurat@gmail.com>
2022-10-31 14:23:30 -0300

    Refatoracao metodos negociais

--- src/main/java/br/com/muratsystems/distanciafacil/domain/service/DistanciaRotaService.java
+++ src/main/java/br/com/muratsystems/distanciafacil/domain/service/DistanciaRotaService.java
@@ -17,39 +17,56 @@ public class DistanciaRotaService {
 	private static final int RAIO_TERRA_KM = 6371;
 
 	public List<DistanciaRota> calcularDistanciaEnderecos(List<Endereco> enderecos) {
-
 		List<int[]> elementosComparados = new ArrayList<>();
 		List<DistanciaRota> distancias = new ArrayList<>();
-
-		for (Endereco enderecoA : enderecos) {
-			int numeroElemA = enderecos.indexOf(enderecoA) + 1;
-			for (Endereco enderecoB : enderecos) {
-				if (!enderecoA.equals(enderecoB)) {
-					int numeroElemB = enderecos.indexOf(enderecoB) + 1;
-
-					boolean jaComparado = false;
-					for (int[] elementos : elementosComparados) {
-						if ((elementos[0] == numeroElemA && elementos[1] == numeroElemB)
-								|| (elementos[0] == numeroElemB && elementos[1] == numeroElemA)) {
-							jaComparado = true;
-							break;
-						}
-					}
-					if (!jaComparado) {
-						var distanciaRota = new DistanciaRota();
-						distanciaRota.setEnderecoA(enderecoA);
-						distanciaRota.setEnderecoB(enderecoB);
-						calcularDistanciaEmKmEntrePontos(distanciaRota);
-						distancias.add(distanciaRota);
-						int[] comparacao = { numeroElemA, numeroElemB };
-						elementosComparados.add(comparacao);
-					}
-				}
-			}
+		for (var enderecoA : enderecos) {
+			compararComEnderecoB(enderecos, enderecoA, elementosComparados, distancias);
 		}
 		return distancias;
 	}
 
+	private void compararComEnderecoB(List<Endereco> enderecos, Endereco enderecoA, List<int[]> elementosComparados,
+			List<DistanciaRota> distancias) {
+		for (var enderecoB : enderecos) {
+			executarComparacao(enderecos, enderecoA, enderecoB, elementosComparados, distancias);
+		}
+	}
+
+	private void executarComparacao(List<Endereco> enderecos, Endereco enderecoA, Endereco enderecoB,
+			List<int[]> elementosComparados, List<DistanciaRota> distancias) {
+		if (!enderecoA.equals(enderecoB)) {
+			int numeroElemA = enderecos.indexOf(enderecoA) + 1;
+			int numeroElemB = enderecos.indexOf(enderecoB) + 1;
+			if (!isElementoComparado(numeroElemA, numeroElemB, elementosComparados)) {
+				definirDistancias(enderecoA, enderecoB, distancias);
+				int[] comparacao = { numeroElemA, numeroElemB };
+				elementosComparados.add(comparacao);
+			}
+		}
+	}
+
+	private boolean isElementoComparado(int numeroElemA, int numeroElemB, List<int[]> elementosComparados) {
+		for (int[] elementos : elementosComparados) {
+			if ((elementos[0] == numeroElemA && elementos[1] == numeroElemB)
+					|| (elementos[0] == numeroElemB && elementos[1] == numeroElemA)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void definirDistancias(Endereco enderecoA, Endereco enderecoB, List<DistanciaRota> distancias) {
+		var distanciaRota = new DistanciaRota();
+		distanciaRota.setEnderecoA(enderecoA);
+		distanciaRota.setEnderecoB(enderecoB);
+		calcularDistanciaEmKmEntrePontos(distanciaRota);
+		distancias.add(distanciaRota);
+	}
+
+	/**
+	 * Solução adaptada.
+	 * Fonte: https://thiagovespa.com.br/blog/2010/09/10/distancia-utilizando-coordenadas-geograficas-em-java/
+	 */
 	public void calcularDistanciaEmKmEntrePontos(DistanciaRota distanciaRota) {
 
 		// Conversão de graus pra radianos das latitudes
--- src/main/java/br/com/muratsystems/distanciafacil/domain/service/GeoLocalizacaoService.java
+++ src/main/java/br/com/muratsystems/distanciafacil/domain/service/GeoLocalizacaoService.java
@@ -14,6 +14,7 @@ import br.com.muratsystems.distanciafacil.domain.exception.BusinessException;
 import br.com.muratsystems.distanciafacil.domain.model.Coordenada;
 import br.com.muratsystems.distanciafacil.domain.model.DistanciaRota;
 import br.com.muratsystems.distanciafacil.domain.model.Endereco;
+import br.com.muratsystems.distanciafacil.geoapify.model.ResultGeocoding;
 import br.com.muratsystems.distanciafacil.geoapify.service.GeoapifyService;
 
 @Service
@@ -27,7 +28,7 @@ public class GeoLocalizacaoService {
 
 	public List<Endereco> definirEnderecos(List<String> enderecosString) {
 		if (enderecosString == null || enderecosString.size() < 3) {
-			throw new BusinessException("Deve ser informado no mínimo 3 endereços!");
+			throw new BusinessException("Devem ser informados no mínimo 3 endereços!");
 		}
 		List<Endereco> enderecos = new ArrayList<>();
 		for (String enderecoString : enderecosString) {
@@ -39,51 +40,59 @@ public class GeoLocalizacaoService {
 	}
 
 	public Optional<Endereco> definirEnderecoComCoordenada(String enderecoString) {
-		var endereco = new Endereco();
 		var optResultado = geoapifyService.getGeocodingGeoapify(enderecoString);
 		if (optResultado.isPresent()) {
-			endereco.setLogradouro(optResultado.get().getStreet());
-			endereco.setNumero(optResultado.get().getHousenumber());
-			endereco.setBairro(optResultado.get().getSuburb());
-			endereco.setCidade(optResultado.get().getCity());
-			endereco.setUf(optResultado.get().getState_code());
-			endereco.setPais(optResultado.get().getCountry());
-			endereco.setCep(optResultado.get().getPostcode());
-			var coordenada = new Coordenada();
-			coordenada.setLatitude(optResultado.get().getLat());
-			coordenada.setLongitude(optResultado.get().getLon());
-			endereco.setCoordenada(coordenada);
-			return Optional.of(endereco);
+			return Optional.of(definirEndereco(optResultado.get()));
 		}
 		throw new BusinessException("Endereço " + enderecoString + " não encontrado no serviço de Geocoding");
+	}
+
+	private Endereco definirEndereco(ResultGeocoding resultado) {
+		var endereco = new Endereco();
+		endereco.setLogradouro(resultado.getStreet());
+		endereco.setNumero(resultado.getHousenumber());
+		endereco.setBairro(resultado.getSuburb());
+		endereco.setCidade(resultado.getCity());
+		endereco.setUf(resultado.getState_code());
+		endereco.setPais(resultado.getCountry());
+		endereco.setCep(resultado.getPostcode());
+		var coordenada = new Coordenada();
+		coordenada.setLatitude(resultado.getLat());
+		coordenada.setLongitude(resultado.getLon());
+		endereco.setCoordenada(coordenada);
+		return endereco;
 	}
 
 	public DistanciaDto definirDistanciaRetorno(List<DistanciaRota> distanciasRota) {
 		if (!distanciasRota.isEmpty()) {
 			var distanciaDto = new DistanciaDto();
-			List<DistanciaRotaDto> distancias = new ArrayList<>();
-			var maisProxima = new DistanciaRota();
-			var maisDistante = new DistanciaRota();
-			for (var rota : distanciasRota) {
-				if (distanciasRota.indexOf(rota) == 0) {
-					maisProxima = rota;
-					maisDistante = rota;
-				} else {
-					if (rota.getDistanciaEntreEnderecos().compareTo(maisProxima.getDistanciaEntreEnderecos()) < 0) {
-						maisProxima = rota;
-					}
-					if (rota.getDistanciaEntreEnderecos().compareTo(maisDistante.getDistanciaEntreEnderecos()) > 0) {
-						maisDistante = rota;
-					}
-				}
-				distancias.add(toDistanciaRotaDto(rota));
-			}
-			distanciaDto.setDistancias(distancias);
-			distanciaDto.setDistanciaMaisProxima(toDistanciaRotaDto(maisProxima));
-			distanciaDto.setDistanciaMaisDistante(toDistanciaRotaDto(maisDistante));
+			classificarRotas(distanciasRota, distanciaDto);
 			return distanciaDto;
 		}
 		throw new BusinessException("Endereços não encontrados no serviço de Geocoding");
+	}
+
+	private void classificarRotas(List<DistanciaRota> distanciasRota, DistanciaDto distanciaDto) {
+		List<DistanciaRotaDto> distancias = new ArrayList<>();
+		var maisProxima = new DistanciaRota();
+		var maisDistante = new DistanciaRota();
+		for (var rota : distanciasRota) {
+			if (distanciasRota.indexOf(rota) == 0) {
+				maisProxima = rota;
+				maisDistante = rota;
+			} else {
+				if (rota.getDistanciaEntreEnderecos().compareTo(maisProxima.getDistanciaEntreEnderecos()) < 0) {
+					maisProxima = rota;
+				}
+				if (rota.getDistanciaEntreEnderecos().compareTo(maisDistante.getDistanciaEntreEnderecos()) > 0) {
+					maisDistante = rota;
+				}
+			}
+			distancias.add(toDistanciaRotaDto(rota));
+		}
+		distanciaDto.setDistancias(distancias);
+		distanciaDto.setDistanciaMaisProxima(toDistanciaRotaDto(maisProxima));
+		distanciaDto.setDistanciaMaisDistante(toDistanciaRotaDto(maisDistante));
 	}
 
 	private DistanciaRotaDto toDistanciaRotaDto(DistanciaRota entity) {
